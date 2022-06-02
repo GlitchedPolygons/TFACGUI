e503af33f94ec49769edb728d3aed412d31f5c2a
Glitched Polygons <37942667+GlitchedPolygons@users.noreply.github.com>
2022-06-02 11:16:19 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,16 +5,6 @@ if [ "$(whoami)" = "root" ]; then
   exit 1
 fi
 
-PREVCC="$CC"
-PREVCXX="$CXX"
-
-if command -v clang &> /dev/null
-then
-    echo "-- Clang found on system, great! Long live LLVM! :D"
-    export CC=clang
-    export CXX=clang++
-fi
-
 REPO=$(dirname "$0")
 rm -rf "$REPO"/out
 mkdir -p "$REPO"/out && cd "$REPO"/out || exit 2
@@ -22,9 +12,6 @@ mkdir -p "$REPO"/out && cd "$REPO"/out || exit 2
 cmake -DBUILD_SHARED_LIBS=Off -DCMAKE_BUILD_TYPE=Release ../src || exit 3
 
 cmake --build . --config Release || exit 4
-
-export CC="$PREVCC"
-export CXX="$PREVCXX"
 
 cd "$REPO" || exit 5
 
